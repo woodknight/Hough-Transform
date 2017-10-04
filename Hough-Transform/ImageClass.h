2c07a129856c580102606ef47807b5434ff88e4a
J <lijiangpku@gmail.com>
2017-10-03 21:29:21 -0700

    stash

---
+++ Hough-Transform/ImageClass.h
@@ -0,0 +1,81 @@
+#ifndef IMAGECLASS_H
+#define IMAGECLASS_H
+#endif // IMAGECLASS_H
+
+// class of image
+
+// class declaration
+
+template <typename T>
+class Image {
+public:
+	size_t width;
+	size_t height;
+	T *pixels;
+
+	Image(size_t width, size_t height);
+	Image(const Image& image) = delete;
+	Image& operator=(const Image&) = delete;
+
+	~Image();
+
+	size_t GetBytesPerPixel();
+	size_t GetSizeInBytes();
+
+	bool operator==(const Image& rhs) const;
+
+protected:
+	Image(size_t width, size_t height, T *pixels);
+};
+
+// class definition
+
+template <typename T>
+Image<T>::Image(size_t width, size_t height)
+	: width{ width }, height{ height }, pixels{ new T[width*height] } 
+{ 
+}
+
+template <typename T>
+Image<T>::~Image() 
+{
+	delete[] pixels;
+}
+
+template <typename T>
+size_t Image<T>::GetBytesPerPixel() 
+{
+	return sizeof(T);
+}
+
+template <typename T>
+size_t Image<T>::GetSizeInBytes() 
+{
+	return width * height * GetBytesPerPixel();
+}
+
+template <typename T>
+bool Image<T>::operator==(const Image& rhs) const 
+{
+	if (width == rhs.width && height == rhs.height) 
+	{
+		for (auto i = 0; i < width*height; ++i) 
+		{
+			if (pixels[i] != rhs.pixels[i]) 
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+template <typename T>
+Image<T>::Image(size_t width, size_t height, T *pixels)
+	: width{ width }, height{ height }, pixels{ pixels } 
+{
+}
